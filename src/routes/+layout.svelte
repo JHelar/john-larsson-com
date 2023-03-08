b9ebb724cb3eb7562f325fec00a60e5b47e7eb3d
John Larsson <john.helarsson@gmail.com>
2023-03-08 22:00:25 +0100

    fix: add page title, fix footer typo

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,8 +2,12 @@
     import '../app.css';
 </script>
 
+<svelte:head>
+    <title>J.H.E.L</title>
+</svelte:head>
+
 <slot />
 <footer class="container mt-12 pb-12 text-xs text-slate-400">
     <p>All rights reserved</p>
-    <p>©2022, John Henrik Elif Larsson</p>
+    <p>©2022, John Henrik Elof Larsson</p>
 </footer>
