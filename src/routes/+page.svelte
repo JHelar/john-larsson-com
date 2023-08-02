756faae42a9d5efd27352dd8e6c1eea413924e58
John Larsson <john.helarsson@gmail.com>
2023-08-02 16:50:26 +0200

    fix: update front page copy

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,18 +8,22 @@
 	<h1 class="text-2xl font-semibold">Hello, I'm John!</h1>
 	<p class="mt-6">
 		I'm a fullstack developer from Sweden, where I am currently working at Telia, specificly with
-		their VOD/SVOD streaming product Teliaplay and C More.
+		their VOD/SVOD content aggregator product <a href="https://teliaplay.se">Teliaplay</a> and
+		<a href="https://cmore.se">C More</a>
 	</p>
 	<p class="mt-6">
-		Today, I'm primarly working as a senior web and systems developer. Before that I worked as a
-		fullstack engineer within the marketing domain at a finctech company. And before that I was part
-		of a small consultant team that focused on creating websites for a multitude of Swedish
-		municipals.
+		Today, I'm currently working as a senior web and systems developer. Before that I worked as a
+		fullstack engineer within the marketing domain at a finctech company. <br />
+		And before that I was part of a small consultant team that focused on creating websites and tools
+		for some of Swedens municipals.
 	</p>
 	<p class="mt-6">
 		I enjoy everything from playing piano, running marathons and starting new code projects that has
-		a lifespan of maximum one month. Projects that can span everywere from fetching and streaming
-		music to creating a new templating engine in Rust.
+		a lifespan of maximum one month (the common curse of a developer it seems!). These projects can
+		span anywere from <a href="https://github.com/JHelar/yaff">creating a HTML templating engine</a>
+		in Rust, a <a href="https://github.com/JHelar/om">functional object mapper Library</a> in
+		Typescript to a nose dive into
+		<a href="https://github.com/JHelar/NotesApp">Kotlin multiplatform.</a>
 	</p>
 	<ul class="mt-6">
 		<li><a href="https://www.linkedin.com/in/john-larsson-8ba758b3/">LinkedIn</a></li>
@@ -30,10 +34,16 @@
 	<h2 class="text-2xl font-semibold">Work</h2>
 	<p class="mt-6">This list contains some of the work that I have done.</p>
 	<ol class="list-decimal list-inside ml-8 mt-6">
-		<li><a href="/work/one-design-system">One Design System Library, Telia</a></li>
-		<li>In app messages, Klarna <small>[soon]</small></li>
-		<li>Block articles, Klarna <small>[soon]</small></li>
-		<li>SiteVision project template engine (svhiq), HiQ <small>[soon]</small></li>
+		<li>
+			One Design System Library, Telia <small>[article coming soon™, ask me about it!]</small>
+		</li>
+		<li>In app messages, Klarna <small>[article coming soon™, ask me about it!]</small></li>
+		<li>Block articles, Klarna <small>[article coming soon™, ask me about it!]</small></li>
+		<li>
+			SiteVision project template engine (svhiq), HiQ <small
+				>[article coming soon™, ask me about it!]</small
+			>
+		</li>
 	</ol>
 	<p class="mt-6">
 		Check out my <a href="https://github.com/JHelar">GitHub</a> profile to see all my public projects.
