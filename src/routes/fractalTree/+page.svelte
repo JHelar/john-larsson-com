d541476adb2d8297dbda84c999b598a7ee3367ad
John Larsson <john.helarsson@gmail.com>
2023-12-11 22:02:02 +0100

    fractal tree controls

--- src/routes/fractalTree/+page.svelte
+++ src/routes/fractalTree/+page.svelte
@@ -1,5 +1,12 @@
 <script lang="ts">
-	import { initialize } from './fractalTree';
+	import {
+		BONZAI_MODE_STORE,
+		BRANCH_COUNT_STORE,
+		MAX_BRANCH_DEPTH_STORE,
+		ROOT_LENGTH_RATIO_STORE,
+		ROOT_THICKNESS_STORE,
+		initialize
+	} from './fractalTree';
 	import Head from '$lib/components/Head.svelte';
 </script>
 
@@ -8,6 +15,50 @@
 <div class="container mb-4 flex flex-col">
 	<div class="mb-4">
 		<h1 class="text-2xl font-semibold mb-2">Fractal tree</h1>
+	</div>
+	<div class="mb-2 flex row gap-x-4 items-end">
+		<div class="flex-1">
+			<label
+				for="default-range"
+				class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+				>Branch depth ({$MAX_BRANCH_DEPTH_STORE})</label
+			>
+			<input
+				id="default-range"
+				type="range"
+				min="1"
+				max="15"
+				bind:value={$MAX_BRANCH_DEPTH_STORE}
+				class="w-full h-2 bg-gray-200 rounded-lg appearance-none cursor-pointer dark:bg-gray-700"
+			/>
+		</div>
+		<div class="flex-1">
+			<label
+				for="default-range"
+				class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+				>Branching factor ({$BRANCH_COUNT_STORE})</label
+			>
+			<input
+				id="default-range"
+				type="range"
+				min="0"
+				max="10"
+				bind:value={$BRANCH_COUNT_STORE}
+				class="w-full h-2 bg-gray-200 rounded-lg appearance-none cursor-pointer dark:bg-gray-700"
+			/>
+		</div>
+		<div class="flex items-center gap-x-2">
+			<input
+				id="default-checkbox"
+				type="checkbox"
+				bind:checked={$BONZAI_MODE_STORE}
+				class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+			/>
+			<label
+				for="default-checkbox"
+				class="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">Bonzai mode</label
+			>
+		</div>
 	</div>
 </div>
 <div class="w-screen m-auto">
--- src/routes/fractalTree/fractalTree.ts
+++ src/routes/fractalTree/fractalTree.ts
@@ -1,3 +1,5 @@
+import { derived, writable } from 'svelte/store';
+
 type Branch = {
 	length: number;
 	goalLength: number;
@@ -6,26 +8,89 @@ type Branch = {
 	y: number;
 	initialY: number;
 	branches: Branch[];
+	hasBranches: boolean;
 	doneAnimating: boolean;
 };
-
-const ROOT_THICKNESS = 20;
-const ROOT_LENGTH_RATIO = 0.3;
-const BRANCH_LENGTH_RATIO = 0.6;
-const BRANCH_THICKNESS_RATIO = 0.8;
-const BRANCH_ANGLE = getRadians(45);
+// Defaults
+const ROOT_THICKNESS = 35;
+const ROOT_LENGTH_RATIO = 0.35;
+const BRANCH_LENGTH_RATIO = 0.65;
+const BRANCH_THICKNESS_RATIO = 0.65;
+const BRANCH_ANGLE = getRadians(50);
 const GROWTH_SPEED = 350;
-const MAX_BRANCH_DEPTH = 10;
-let tree: Branch;
+const MAX_BRANCH_DEPTH = 7;
+const BRANCH_COUNT = 6;
+const BONZAI_MODE = true;
 
+// Constants
+const CONSTANTS = {
+	ROOT_THICKNESS,
+	ROOT_LENGTH_RATIO,
+	BRANCH_LENGTH_RATIO,
+	BRANCH_THICKNESS_RATIO,
+	BRANCH_ANGLE,
+	GROWTH_SPEED,
+	MAX_BRANCH_DEPTH,
+	BRANCH_COUNT,
+	BONZAI_MODE
+};
+
+let tree: Branch;
+let runUpdate = true;
+let invalidate = false;
 let prevTime = 0;
+
+export const ROOT_THICKNESS_STORE = writable(CONSTANTS.ROOT_THICKNESS);
+export const ROOT_LENGTH_RATIO_STORE = writable(CONSTANTS.ROOT_LENGTH_RATIO);
+export const BRANCH_LENGTH_RATIO_STORE = writable(CONSTANTS.BRANCH_LENGTH_RATIO);
+export const BRANCH_THICKNESS_RATIO_STORE = writable(CONSTANTS.BRANCH_THICKNESS_RATIO);
+export const BRANCH_ANGLE_STORE = writable(CONSTANTS.BRANCH_ANGLE);
+export const GROWTH_SPEED_STORE = writable(CONSTANTS.GROWTH_SPEED);
+export const MAX_BRANCH_DEPTH_STORE = writable(CONSTANTS.MAX_BRANCH_DEPTH);
+export const BRANCH_COUNT_STORE = writable(CONSTANTS.BRANCH_COUNT);
+export const BONZAI_MODE_STORE = writable(CONSTANTS.BONZAI_MODE);
+ROOT_THICKNESS_STORE.subscribe((value) => (CONSTANTS.ROOT_THICKNESS = value));
+ROOT_LENGTH_RATIO_STORE.subscribe((value) => (CONSTANTS.ROOT_LENGTH_RATIO = value));
+BRANCH_LENGTH_RATIO_STORE.subscribe((value) => (CONSTANTS.BRANCH_LENGTH_RATIO = value));
+BRANCH_THICKNESS_RATIO_STORE.subscribe((value) => (CONSTANTS.BRANCH_THICKNESS_RATIO = value));
+BRANCH_ANGLE_STORE.subscribe((value) => (CONSTANTS.BRANCH_ANGLE = value));
+GROWTH_SPEED_STORE.subscribe((value) => (CONSTANTS.GROWTH_SPEED = value));
+MAX_BRANCH_DEPTH_STORE.subscribe((value) => (CONSTANTS.MAX_BRANCH_DEPTH = value));
+BRANCH_COUNT_STORE.subscribe((value) => (CONSTANTS.BRANCH_COUNT = value));
+BONZAI_MODE_STORE.subscribe((value) => {
+	if (tree) {
+		tree.doneAnimating = false;
+		tree.branches.splice(0);
+	}
+	CONSTANTS.BONZAI_MODE = value;
+});
+const INVALIDATE_STORE = derived(
+	[
+		ROOT_THICKNESS_STORE,
+		ROOT_LENGTH_RATIO_STORE,
+		BRANCH_LENGTH_RATIO_STORE,
+		BRANCH_THICKNESS_RATIO_STORE,
+		BRANCH_ANGLE_STORE,
+		GROWTH_SPEED_STORE,
+		MAX_BRANCH_DEPTH_STORE,
+		BRANCH_COUNT_STORE,
+		BONZAI_MODE_STORE
+	],
+	() => {
+		invalidate = true;
+	}
+	// eslint-disable-next-line @typescript-eslint/no-empty-function
+).subscribe(() => {});
 
 function startLoop(context: CanvasRenderingContext2D) {
 	function runLoop(timeframe: number) {
 		const deltaTime = (timeframe - prevTime) / 1000;
 		prevTime = timeframe;
 
-		update(deltaTime);
+		if (runUpdate || invalidate) {
+			update(deltaTime, invalidate);
+			invalidate = false;
+		}
 		render(context);
 		requestAnimationFrame(runLoop);
 	}
@@ -40,76 +105,123 @@ function startLoop(context: CanvasRenderingContext2D) {
 function getRadians(degrees: number) {
 	return (degrees * Math.PI) / 180;
 }
-
-function renderBranch(
-	branch: Branch,
-	context: CanvasRenderingContext2D,
-	currentBranchDepth: number
+function translate(
+	value: number,
+	[oldMin, oldMax]: [number, number],
+	[newMin, newMax]: [number, number]
 ) {
-	context.fillStyle = currentBranchDepth >= MAX_BRANCH_DEPTH * 0.6 ? '#034F1B' : '#6F371F';
-	context.fillRect(branch.x, branch.y, branch.thickness, branch.length);
+	return ((value - oldMin) * (newMax - newMin)) / (oldMax - oldMin) + newMin;
+}
 
-	if (branch.doneAnimating) {
-		context.translate(branch.x + branch.thickness / 2, branch.y);
-		context.rotate(BRANCH_ANGLE);
-		context.translate(-(branch.x + branch.thickness / 2), -branch.y);
+function renderBranch(root: Branch, context: CanvasRenderingContext2D, currentBranchDepth: number) {
+	context.fillStyle =
+		currentBranchDepth >= CONSTANTS.MAX_BRANCH_DEPTH * 0.6 ? '#034F1B' : '#6F371F';
+	context.fillRect(root.x, root.y, root.thickness, root.length);
 
-		renderBranch(branch.branches[0], context, currentBranchDepth + 1);
+	if (root.doneAnimating) {
+		root.branches.forEach((branch, i) => {
+			const rotation = translate(
+				i,
+				[0, root.branches.length - 1],
+				[CONSTANTS.BRANCH_ANGLE, -CONSTANTS.BRANCH_ANGLE]
+			);
 
-		context.translate(branch.x + branch.thickness / 2, branch.y);
-		context.rotate(-BRANCH_ANGLE);
-		context.translate(-(branch.x + branch.thickness / 2), -branch.y);
+			context.translate(root.x + root.thickness / 2, root.y);
+			context.rotate(rotation);
+			context.translate(-(root.x + root.thickness / 2), -root.y);
 
-		context.translate(branch.x + branch.thickness / 2, branch.y);
-		context.rotate(-BRANCH_ANGLE);
-		context.translate(-(branch.x + branch.thickness / 2), -branch.y);
+			renderBranch(branch, context, currentBranchDepth + 1);
 
-		renderBranch(branch.branches[1], context, currentBranchDepth + 1);
-
-		context.translate(branch.x + branch.thickness / 2, branch.y);
-		context.rotate(BRANCH_ANGLE);
-		context.translate(-(branch.x + branch.thickness / 2), -branch.y);
+			context.translate(root.x + root.thickness / 2, root.y);
+			context.rotate(-rotation);
+			context.translate(-(root.x + root.thickness / 2), -root.y);
+		});
 	}
 }
 
-function updateBranch(branch: Branch, deltaTime: number, currentBranchDepth: number) {
-	if (currentBranchDepth > MAX_BRANCH_DEPTH) return;
+function updateBranch(
+	root: Branch,
+	deltaTime: number,
+	currentBranchDepth: number,
+	invalidate: boolean
+) {
+	if (currentBranchDepth > CONSTANTS.MAX_BRANCH_DEPTH) {
+		root.branches.splice(0);
+		root.hasBranches = false;
+		return true;
+	}
+	let depthReached = false;
 
-	const length = Math.min(branch.goalLength, branch.length + GROWTH_SPEED * deltaTime);
-	branch.length = length;
-	branch.y = branch.initialY - length;
+	const length = Math.min(root.goalLength, root.length + CONSTANTS.GROWTH_SPEED * deltaTime);
+	root.length = length;
+	root.y = root.initialY - length;
 
-	if (length === branch.goalLength) {
-		branch.doneAnimating = true;
-		if (branch.branches.length) {
-			branch.branches.forEach((child) => updateBranch(child, deltaTime, currentBranchDepth + 1));
+	if (length === root.goalLength) {
+		root.doneAnimating = true;
+		if (root.hasBranches && !invalidate) {
+			root.branches.forEach((child) => {
+				depthReached = updateBranch(child, deltaTime, currentBranchDepth + 1, invalidate);
+			});
+		} else if (root.hasBranches && invalidate) {
+			root.doneAnimating = false;
+			const branchDiff =
+				(CONSTANTS.BONZAI_MODE
+					? 1 + Math.round(Math.random() * CONSTANTS.BRANCH_COUNT)
+					: CONSTANTS.BRANCH_COUNT) - root.branches.length;
+			// Cut branches
+			if (branchDiff < 0) {
+				root.branches.splice(0, Math.abs(branchDiff));
+			} else if (branchDiff > 0) {
+				Array(branchDiff)
+					.fill(0)
+					.forEach(() => {
+						const thickness = root.thickness * CONSTANTS.BRANCH_THICKNESS_RATIO;
+						root.branches.push({
+							branches: [],
+							hasBranches: false,
+							doneAnimating: false,
+							goalLength: length * CONSTANTS.BRANCH_LENGTH_RATIO,
+							initialY: root.y,
+							length: 0,
+							thickness: root.thickness * CONSTANTS.BRANCH_THICKNESS_RATIO,
+							x: root.x + root.thickness - thickness - (root.thickness - thickness) / 2,
+							y: 0
+						});
+					});
+			}
+			root.branches.forEach((child) => {
+				depthReached = updateBranch(child, deltaTime, currentBranchDepth + 1, invalidate);
+			});
 		} else {
-			branch.branches.push({
-				branches: [],
-				doneAnimating: false,
-				goalLength: length * BRANCH_LENGTH_RATIO,
-				initialY: branch.y,
-				length: 0,
-				thickness: branch.thickness * BRANCH_THICKNESS_RATIO,
-				x: branch.x,
-				y: 0
-			});
-			branch.branches.push({
-				branches: [],
-				doneAnimating: false,
-				goalLength: length * BRANCH_LENGTH_RATIO,
-				initialY: branch.y,
-				length: 0,
-				thickness: branch.thickness * BRANCH_THICKNESS_RATIO,
-				x: branch.x,
-				y: 0
-			});
+			root.hasBranches = true;
+			const branchCount = CONSTANTS.BONZAI_MODE
+				? 1 + Math.round(Math.random() * CONSTANTS.BRANCH_COUNT)
+				: CONSTANTS.BRANCH_COUNT;
+			Array(branchCount)
+				.fill(0)
+				.forEach(() => {
+					const thickness = root.thickness * CONSTANTS.BRANCH_THICKNESS_RATIO;
+					root.branches.push({
+						branches: [],
+						hasBranches: false,
+						doneAnimating: false,
+						goalLength: length * CONSTANTS.BRANCH_LENGTH_RATIO,
+						initialY: root.y,
+						length: 0,
+						thickness: root.thickness * CONSTANTS.BRANCH_THICKNESS_RATIO,
+						x: root.x + root.thickness - thickness - (root.thickness - thickness) / 2,
+						y: 0
+					});
+				});
 		}
 	}
+
+	return depthReached;
 }
 
-function update(deltaTime: number) {
-	updateBranch(tree, deltaTime, 0);
+function update(deltaTime: number, invalidate: boolean) {
+	const depthReached = updateBranch(tree, deltaTime, 0, invalidate);
+	// runUpdate = !depthReached;
 }
 
 function render(context: CanvasRenderingContext2D) {
@@ -127,12 +239,13 @@ export function initialize(canvas: HTMLCanvasElement) {
 	context.canvas.width = context.canvas.parentElement?.clientWidth ?? 0;
 	context.canvas.height = context.canvas.width * (9 / 16);
 
-	const branchLength = context.canvas.height * ROOT_LENGTH_RATIO;
+	const branchLength = context.canvas.height * CONSTANTS.ROOT_LENGTH_RATIO;
 	tree = {
 		goalLength: branchLength,
 		length: 0,
-		thickness: ROOT_THICKNESS,
-		x: context.canvas.width / 2 - ROOT_THICKNESS / 2,
+		hasBranches: false,
+		thickness: CONSTANTS.ROOT_THICKNESS,
+		x: context.canvas.width / 2 - CONSTANTS.ROOT_THICKNESS / 2,
 		y: 0,
 		initialY: canvas.height,
 		branches: [],
